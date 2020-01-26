87c35e38e0cf5e1f199ba7e12fcc2dc711b525cd
emilie <emiholtz@iu.edu>
2020-01-26 17:10:44 -0500

    Starting tutorial, creating  display for room text. https://www.youtube.com/watch?v=TP4gLQmKOLo&list=FLd6GU25H9gw84J97B-HuI3g&index=10&t=0s STOPPED: 8:44

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    [HideInInspector]
+    public Navigation roomNavigate;
+
+    void Awake()
+    {
+        roomNavigate = GetComponent<Navigation>();
+    }
+
+    public void DisplayRoomText()
+    {
+        string combinedText = roomNavigate.currentRoom.description + "\n";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Text Adventure/Room")]
+public class Room : ScriptableObject
+{
+    //Scriptable Object
+    //Never get attached to game objects, exist as assets and run in memory, etc.
+    [TextArea]
+    public string description;
+    public string roomID;
+
+}
