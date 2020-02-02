929c162dabe354d52141f45336b1a5e2b8406ad6
emilie <emiholtz@iu.edu>
2020-02-02 13:49:05 -0500

    https://www.youtube.com/watch?v=1qbjmb_1hV4&feature=emb_title typing tutorial https://www.youtube.com/watch?v=-LlAahTMtjw next part

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,26 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
 
+    public TMP_Text displayText;
+
     [HideInInspector]
-    public Navigation roomNavigate;
+    public Navigation roomNavigation;
+
+    List<string> actionLog = new List<string>();
 
     void Awake()
     {
-        roomNavigate = GetComponent<Navigation>();
+        roomNavigation = GetComponent<Navigation>();
+    }
+
+    void Start()
+    {
+        DisplayRoomText();
+        DisplayLoggedText();
+    }
+
+    public void DisplayLoggedText()
+    {
+        //Displays everything in the action log currently.
+        string logAsText = string.Join("\n", actionLog.ToArray());
+        displayText.text = logAsText;
     }
 
     public void DisplayRoomText()
     {
-        string combinedText = roomNavigate.currentRoom.description + "\n";
+        //Displays Room's individual text.
+        string combinedText = roomNavigation.currentRoom.description + "\n";
+
+        LogStringWithReturn(combinedText);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LogStringWithReturn(string stringToAdd)
     {
-        
+        actionLog.Add(stringToAdd + "\n");
     }
 }
