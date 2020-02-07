2de68c55facff0599a09ee555a3d04e7287d15ce
Emilie <emiholtz@iu.edu>
2020-02-07 16:15:49 -0500

    https://www.youtube.com/watch?v=g9Pjv-BWpso&list=PLX2vGYjWbI0RfcpqpKlmLEy7NteIog8g4&index=6 Next Part

---
+++ Assets/Scripts/TextInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextInput : MonoBehaviour
+{
+    private GameController controller;
+    public TMP_InputField inputField;
+
+    void Awake()
+    {
+        controller = GetComponent<GameController>();
+        //call the function any time onEndEdit event occurs, eg. Presses return/enter
+        inputField.onEndEdit.AddListener(AcceptStringInput);
+    }
+    void AcceptStringInput(string userInput)
+    {
+        userInput = userInput.ToLower();
+        controller.LogStringWithReturn(userInput);
+        InputComplete();
+    }
+
+    void InputComplete()
+    {
+        controller.DisplayLoggedText();
+        inputField.ActivateInputField();
+        inputField.text = null;
+    }
+}
